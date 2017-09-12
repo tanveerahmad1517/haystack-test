6e8378a8a404e0908bcc8c872efc3943ac7331bc
Thomas Cairnes <tcairnes@ariel.badc.rl.ac.uk>
2017-09-12 09:45:28 +0100

    Basic indexing and minimal search

---
+++ testsite/test.py
@@ -0,0 +1,22 @@
+from datetime import datetime as dt
+import django
+django.setup()
+
+from haystackWork.models import Note
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+
+user = User(username='you', email="your.mail@your.mail.com")
+user.set_password('you')
+user.save()
+
+titles = ['thing', 'other thing', 'stuff', 'whatever', 'nothing']
+bodies = ['Lots of writing about {title}'.format(title=title) for title in titles]
+
+# Make some data
+for i in range(5):
+    note = Note(user=user, 
+                pub_date=dt.now(),
+                title=titles[i],
+                body=bodies[i])
+    note.save()
