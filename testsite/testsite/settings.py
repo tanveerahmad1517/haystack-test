6fc8397ef4b6f195544a40b719ccb3926a490615
Ag Stephens <ag.stephens@stfc.ac.uk>
2017-09-24 12:14:43 +0000

    Removed venv directory and added settings_local.py

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from settings_local import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,15 +84,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack_elasticsearch.elasticsearch5.Elasticsearch5SearchEngine',
-        'URL': 'http://jasmin-es-test.ceda.ac.uk:9200/',
-        'INDEX_NAME': 'haystack',
-    },
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
